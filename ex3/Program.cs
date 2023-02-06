990d2c29ae4541183b729f501b48de577268cb2d
SlavaMSV <viacheslav.maslov@gmail.com>
2023-02-06 22:22:02 +0300

    Добавлена программа вывода на экран массива из 8 элементов

---
+++ ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+
+int[]arr=new int[8];
+Random rnd = new Random();
+for (int i=0; i<arr.Length; i++)
+{
+    arr[i]=rnd.Next(0,189);
+    Console.Write($"{arr[i]}  ");
+}
+
+
+
