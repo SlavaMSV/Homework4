e7bd6d951d492097d21e982c55cc2817c4e7b3cb
SlavaMSV <viacheslav.maslov@gmail.com>
2023-02-06 22:01:54 +0300

    Добавлена программа нахождения суммы цифр натурального числа

---
+++ ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int Getnumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result>=0)
+        {
+            break;
+        }
+        else
+        {
+        Console.WriteLine("Введено не число или не корректное число");
+        }
+    }
+    return result;
+}
+void Sumdigit(int number) 
+{ 
+int sum=0;
+    for(int num = number; num>0; num/=10)
+    {
+        sum+=num%10;
+    }
+        Console.WriteLine($"Сумма цифр числа {number} равна {sum}");
+}
+int number=Getnumber("Введите натуральное число:");
+Sumdigit(number);
